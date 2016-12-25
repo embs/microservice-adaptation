80366cb47b1b7d101131acd3e03ca843911f1d9c
Adalberto Junior <adalrsjr1@gmail.com>
2016-12-25 18:03:18 -0300

    ms-http-client updated

--- ms-http-client/src/main/java/br/cin/gfads/adalrsjr1/mshttpclient/App.groovy
+++ ms-http-client/src/main/java/br/cin/gfads/adalrsjr1/mshttpclient/App.groovy
@@ -1,18 +1,21 @@
-package br.cin.gfads.adalrsjr1.mshttpclient;
+package br.cin.gfads.adalrsjr1.mshttpclient
 
 import groovy.util.logging.Slf4j
 
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 import org.eclipse.jetty.server.Server
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.servlet.ServletContextHandler
+import org.eclipse.jetty.servlet.ServletHolder
 import org.glassfish.jersey.server.ResourceConfig
 import org.glassfish.jersey.servlet.ServletContainer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
+import com.google.common.base.Stopwatch
+import com.google.common.base.Stopwatch
 import com.google.common.util.concurrent.RateLimiter
 
 public class App {
@@ -20,18 +23,20 @@ public class App {
 	
 	public static void main( String[] args ) {
 		
-		Map env = System.getenv();
+		Map env = System.getenv()
 		
-		String destination = env.getOrDefault("SERVER", "localhost");
-		int rate = Integer.parseInt(env.getOrDefault("THROTTLE", "1")) % 1000;
-		
-		int cores = Runtime.getRuntime().availableProcessors();
+		String destination = env.getOrDefault("SERVER", "localhost")
+		int rate = Integer.parseInt(env.getOrDefault("THROTTLE", "1")) % 1000
+		long MAX_TIME = Long.parseLong(env.getOrDefault("MAX_TIME", "10"))
+		int cores = Runtime.getRuntime().availableProcessors()
 		
 		ExecutorService tPool = Executors.newWorkStealingPool()//Executors.newFixedThreadPool((int)Math.round(rate/2)+1 * cores);
-		RateLimiter throttle = RateLimiter.create(rate);
+		RateLimiter throttle = RateLimiter.create(rate)
 		
-		for(;;) {
-			throttle.acquire();
+		
+		Stopwatch watch = Stopwatch.createStarted()
+		while(watch.elapsed(TimeUnit.SECONDS) <= MAX_TIME) {
+			throttle.acquire()
 			tPool.execute( {
 				try {
 				UUID uuid = UUID.randomUUID()
@@ -41,7 +46,7 @@ public class App {
 				
 				BufferedReader buffReader = new BufferedReader(new InputStreamReader(connection.getInputStream()))
 				
-				String input;
+				String input
 				while((input = buffReader.readLine()) != null) {
 					log.info input
 				}
@@ -50,7 +55,8 @@ public class App {
 				catch(Exception e) {
 					log.error e.getMessage()
 				}
-			});
+			})
 		}
+		watch.stop()
 	}
 }

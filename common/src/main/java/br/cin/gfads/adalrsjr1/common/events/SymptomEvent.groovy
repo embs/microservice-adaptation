02dd28962cc540ccb0ad78bf117bcdc76cecb876
Adalberto Junior <adalrsjr1@gmail.com>
2017-01-23 18:23:00 -0300

    symptom bug

--- common/src/main/java/br/cin/gfads/adalrsjr1/common/events/SymptomEvent.groovy
+++ common/src/main/java/br/cin/gfads/adalrsjr1/common/events/SymptomEvent.groovy
@@ -16,7 +16,7 @@ public class SymptomEvent extends CommonEvent {
 	@JsonProperty("content")
 	private Map content
 	
-	public long TIME 
+	public Long TIME 
 	
 	public SymptomEvent() { 
 		super()
@@ -29,7 +29,9 @@ public class SymptomEvent extends CommonEvent {
 	}
 	
 	public SymptomEvent(byte[] message) {
-		super(init(message))
+		super("")
+		super.source = init(message)
+		
 	}
 	
 //	public SymptomEvent(Object source, Map content) {
@@ -67,11 +69,15 @@ public class SymptomEvent extends CommonEvent {
 		ObjectMapper mapper2 = new ObjectMapper()
 		Map<String,String> javaLog = [:]
 		
-		javaLog = mapper.readValue(dockerLog["log"], new TypeReference<Map<String, String>>(){})
+		def value = dockerLog["log"]
+		if(value)
+			javaLog = mapper.readValue(value, new TypeReference<Map<String, String>>(){})
 		
 		ObjectMapper mapper3 = new ObjectMapper()
 		Map<String, String> appLog = [:]
-		appLog = mapper.readValue(javaLog.get("message"), new TypeReference<Map<String, String>>(){})
+		value = javaLog.get("message")
+		if(value)
+			appLog = mapper.readValue(javaLog.get("message"), new TypeReference<Map<String, String>>(){})
 		
 		javaLog['message'] = appLog
 		dockerLog['log'] = javaLog
@@ -81,6 +87,8 @@ public class SymptomEvent extends CommonEvent {
 	
 	@Override
 	byte[] serialize() {
-		throw new RuntimeException("This class cannot be serilized")
+		ObjectMapper mapper = new ObjectMapper()
+		return mapper.writeValueAsBytes(this)
+//		throw new RuntimeException("This class cannot be serilized")
 	}
 }
